878eb92ff13767907b0d29a83195d95e86ffe7dc
JOSH-ROBERTS-PC\joshr <josro14@gmail.com>
2018-05-02 23:49:33 -0600

    added server code to start up server, and installed necessary node packages

---
+++ app/server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8500;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
